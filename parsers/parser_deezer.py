cf15f7d006d2945952db4ee5db285558b5c4118c
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2021-09-23 00:47:01 +0300

    finished search info from the deezer (without the transformation it to the sth worth etc. df); first version

--- parsers/parser_deezer.py
+++ parsers/parser_deezer.py
@@ -72,14 +72,18 @@ class ParserDeezer:
                 return await r.text()
         return link
 
-    async def make_html_links(self, session:object, links:list) -> list:
+    async def make_html_links(self, session:object, links:list, value_multiple:bool=False) -> list:
         """
         Async method which is dedicated to return values of the html for selected values        
         Input:  session = session of the aiohttp values
                 links = list with calculated links
+                value_multiple = boolean value for the multiple searches
         Output: list of the presset html values of them
         """
-        tasks = [asyncio.create_task(self.parse_deezer_manually_link(session, link)) for link, *_ in links]
+        if not value_multiple:
+            tasks = [asyncio.create_task(self.parse_deezer_manually_link(session, link)) for link, *_ in links]
+        else:
+            tasks = [asyncio.create_task(self.parse_deezer_manually_link(session, link)) for link in links]
         return await asyncio.gather(*tasks)
 
     @staticmethod
@@ -91,8 +95,6 @@ class ParserDeezer:
         """
         value_return = [value_string]
         if " (Remastered)" in value_string and value_string[-13:] == " (Remastered)":
-            # print(value_string[:-13])
-            # print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
             value_return.append(value_string[:-13])
         elif " (Remastered)" not in value_string or  (" (Remastered)" in value_string and value_string[-13:] != " (Remastered)"):
             value_return.append(value_string + " (Remastered)")
@@ -115,8 +117,8 @@ class ParserDeezer:
         soup = BeautifulSoup(html, "html.parser")
         check = soup.find("div", {"class":"hidden"})
         check = check.find("script")
-        check = check.text if check else ''
-        if 'window.__DZR_APP_STATE__ = ' in check:
+        check = str(check).split('</script>')[0]  if check else ''
+        if check:
             value_list = json.loads(check.split('window.__DZR_APP_STATE__ = ')[-1])
             value_list = value_list.get("ALBUM", {})
             value_list = value_list.get('data', [])
@@ -144,13 +146,12 @@ class ParserDeezer:
         """
         append_successful, append_bad = [], []
         for result in results:
+            value_dates = [int(i.get('ORIGINAL_RELEASE_DATE', '-4000')[:4]) for i in result]
             if search_best:
-                value_dates = [i.get('ORIGINAL_RELEASE_DATE')[:3] for i in result]
                 value_get = [i for i, date in zip(result, value_dates) if bool(i.get('Checked Album', False) 
                                 and i.get('Checked Artist', False) and date == i.get('Year Searched', ''))]
             else:
-                value_get = [i for i in result if bool(i.get('Checked Album', False) 
-                                                and i.get('Checked Artist', False))]
+                value_get = [i for i, date in zip(result, value_dates) if bool(i.get('Checked Artist', False) and date == i.get('Year Searched', ''))]
             value_get = {} if not value_get else value_get[0]
             if sub_artist and not sub_album:
                 value_fail = [f for f in result if f != value_get and f.get('Checked Artist', False)]
@@ -165,10 +166,67 @@ class ParserDeezer:
             append_bad.append(value_fail)
         return append_successful, append_bad
 
-    
+    async def produce_search_album_songs_parameters(self, html:str, link:str) -> dict:
+        """
+        Async method which is dedicated to get values of the songs to get from it
+        Input:  html = parsed html value from the got values
+                link = link value which was previously parsed
+        Output: dictionary value with all previously got information
+        """
+        value_dict = {}
+        if len(html) < 1000:
+            return value_dict
+        soup = BeautifulSoup(html, "html.parser")
+        soup_head = soup.find(id="naboo_album_head")
+        album_name = soup_head.find('h1').text.strip()
+        album_track_number = soup.find('meta', {"itemprop":"numTracks"}).get("content", '0')
+
+        album_artist = soup_head.find(id="naboo_album_artist")
+        album_artist = [f.text.strip() for f in album_artist.find_all('span')]
+        if len(album_artist) > 1:
+            album_artist, album_year_artist, *_ = album_artist
+        elif len(album_artist) == 1:
+            album_artist, album_year_artist = album_artist[0], ''
+        else:
+            album_artist, album_year_artist = '', ''
+        album_label = soup_head.find("div", {"class": "naboo-album-label label"})
+        album_label = album_label.text.strip() if album_label else ''
+
+        soup_main = soup.find("div", {"class": "naboo_album_content"})
+        album_duration = soup_main.find(id="naboo_album_duration")
+        album_duration = album_duration.text.strip() if album_duration else ''
+        album_duration = album_duration.split('Total duration:')[-1] if 'Total duration:' in album_duration else album_duration
+        
+        album_tracks = soup_main.find_all("td", {"class": "track"})
+        album_tracks = [(a.text.strip(), a.find("a", {"class":"evt-click"}).get('href', '')) for a in album_tracks]
+        
+        album_length = [f.text.strip() for f in soup_main.find_all("td", {"class": "added"})]
+        album_artist_song = [f.text.strip() for f in soup_main.find_all("td", {"class": "artist"})]
+        album_mark = [f.get('title', '0') for f in soup_main.find_all("td", {"class": "popularity"})]
+        album_mark = [f.split('By popularity:')[-1] for f in album_mark]
+
+        value_dict['Album Number Tracks'] = album_track_number
+        value_dict['Album Name'] = album_name
+        value_dict['Album Artist'] = album_artist
+        value_dict['Album Year'] = album_year_artist
+        value_dict['Album Label'] = album_label
+        value_dict['Album Duration'] = album_duration
+        
+        list_songs = []
+        for (name, link), length, author, popularity in zip(album_tracks, album_length, album_artist_song, album_mark):
+            tmp_dict = {}
+            tmp_dict['Song Name'] = name
+            tmp_dict['Song Link'] = link
+            tmp_dict['Song Length'] = length
+            tmp_dict['Song Author'] = author
+            tmp_dict['Song Popularity'] = popularity
+            list_songs.append(tmp_dict)
+        value_dict['Song Parameters'] = list_songs
+
+        return value_dict
 
     async def produce_search_albums_deezer(self, value_artists:list, value_albums:list, value_years:list, 
-                                        search_best:bool=False, sub_artist:bool=True, sub_album:bool=False) -> list:
+                                        search_best:bool=False, sub_artist:bool=True, sub_album:bool=False) -> set:
         """
         Method which is dedicated to return values of the selected values and transform them further
         Input:  value_artists = lists of selected artists
@@ -178,6 +236,7 @@ class ParserDeezer:
                 sub_album = add sub album operating systems
         Output: list with dictionaries of the selected values
         """
+        list_non_found = []
         links = await self.get_links_values_albums(value_artists, value_albums)
         
         #TODO think about adding remaster values of the album concrete here
@@ -186,14 +245,18 @@ class ParserDeezer:
             async with aiohttp.ClientSession(trust_env=True) as session:
                 value_return = await self.make_html_links(session, links)
         tasks = [asyncio.create_task(self.produce_basic_json_results_album(value_html, link, album, artist, year)) 
-                for value_html, (link, album, artist), year in zip(value_return[:1], links[:1], value_years[:1])]
+                for value_html, (link, album, artist), year in zip(value_return, links, value_years)]
         results = await asyncio.gather(*tasks)
-        # pprint(results)
-        # print('44444444444444444444444444444444444444444444444444')
-        # async with semaphore:
         async with semaphore:
             value_list_successfull, value_list_possible = await self.produce_successfull_failed(results, search_best, sub_artist, sub_album)
-            pprint(value_list_successfull[0])
-            k = requests.get(value_list_successfull[0].get('Link Album', 'Untitled')).text
-            soup = BeautifulSoup(k, "html.parser")
-            # print(soup)
+            links_successfull = []
+            for index, i in enumerate(value_list_successfull):
+                if i: 
+                    links_successfull.append(i.get('Link Album'))
+                else: 
+                    list_non_found.append(links[index])
+            async with aiohttp.ClientSession(trust_env=True) as session:
+                value_return = await self.make_html_links(session, links_successfull, True)
+        tasks = [asyncio.create_task(self.produce_search_album_songs_parameters(html, link)) for html, link in zip(value_return, links_successfull)]
+        parsed_albums = await asyncio.gather(*tasks)
+        return parsed_albums, value_list_successfull, value_list_possible, list_non_found
