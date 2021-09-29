a2172c89a9a4fa15a9f300a071f9fc9712fa7793
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2021-09-29 19:16:32 +0300

    added values for the getting webdriver

---
+++ parsers/parse_webdriver.py
@@ -0,0 +1,9 @@
+import os
+import tarfile
+
+class ParseWebDriver:
+    """
+    class which is dedicated to work with selenium and install selenium manually
+    """
+    def __init__(self) -> None:
+        pass
