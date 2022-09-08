7d553d1efbae8e8f4f1a28720384d322040c1870
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-09-08 23:42:35 +0300

    created canvas for the deezer scraping

--- bot_music/bot_music/spiders/run_deezer_listing.py
+++ bot_music/bot_music/spiders/run_deezer_listing.py
@@ -1,12 +1,27 @@
-from scrapy import Spider, Request
+import re
+import json
+from pprint import pprint
+from scrapy import (
+    Spider, 
+    Request,
+)
 
+
+#TODO add here several names
 
 class DeezerListing(Spider):
     """
     
     """
-    def __init__(self, name=None, **kwargs):
-        self._urls = [] #TODO change it later
+    name = 'deezer_listing'
+    def __init__(self, name="search_test", search_type:str="track"):
+        self._name = name
+        self._urls = [
+            "https://www.deezer.com/search/bones",
+        ] #TODO change it later
+
+    def _develop_() -> object:
+        pass
 
     def start_requests(self):
         for url in self._urls:
@@ -18,6 +33,27 @@ class DeezerListing(Spider):
                 cb_kwargs=None, #TODO add later
             )
 
+    @staticmethod
+    def _develop_json(html:str) -> dict:
+        if (dct:=re.search(
+            r'(?<=<script>window.__DZR_APP_STATE__ = ).*?(?=<\/script>)',
+            html,
+        )) and (dct:=dct.group()):
+            return json.loads(dct)
+        return {}
+
+    def parse_album(self, response, query):
+        pass
+
+    def parse_artist(self, response, query):
+        pass
+
     def parse(self, response):
-        print(response.status)
-        
+        if response.status == 200:
+            dct = self._develop_json(response.text)
+        elif response.status == 403:
+            dct = self._develop_json('')
+        print(dct.keys())
+        # print(k:=dct.get("TRACK"))
+        # print(k.keys())
+        print(k:=dct.get("ALBUM"))
---
+++ bot_music/parse_utilities/selenum_scraper.py
@@ -0,0 +1,1 @@
+#TODO continue work from here
